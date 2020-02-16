32423e0f2ab38163c09e3034ad5b0a03b239ea40
emortlock <edward@mortlock.dev>
2020-02-16 08:23:56 +0000

    docs: corrected dir

--- ops/docs.js
+++ ops/docs.js
@@ -3,13 +3,21 @@ const ghpages = require('gh-pages')
 const { version } = require('../package.json')
 const { ACTIONS_DEPLOY_KEY, GITHUB_ACTOR, GITHUB_REPOSITORY } = process.env
 
-ghpages.publish('dist', {
-  branch: 'gh-pages',
-  repo: `https://${ACTIONS_DEPLOY_KEY}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git`,
-  message: `docs: v${version} release`,
-  user: {
-    name: GITHUB_ACTOR,
-    email: `${GITHUB_ACTOR}@users.noreply.github.com`,
+ghpages.publish(
+  'docs',
+  {
+    branch: 'gh-pages',
+    repo: `https://${ACTIONS_DEPLOY_KEY}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git`,
+    message: `docs: v${version} release`,
+    user: {
+      name: GITHUB_ACTOR,
+      email: `${GITHUB_ACTOR}@users.noreply.github.com`,
+    },
+    silent: true,
   },
-  silent: true,
-})
+  err => {
+    if (err) {
+      console.error(err)
+    }
+  },
+)
