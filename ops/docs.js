5171f905ee7d5cd6b1cbf325e9165f30457b9b4e
emortlock <edward@mortlock.dev>
2020-02-16 21:39:56 +0000

    docs: corrected ghpages auth

--- ops/docs.js
+++ ops/docs.js
@@ -1,13 +1,14 @@
 const ghpages = require('@emortlock/gh-pages')
 const path = require('path')
 const { version } = require('../package.json')
-const { ACTIONS_DEPLOY_KEY, GITHUB_ACTOR, GITHUB_REPOSITORY } = process.env
+const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_ACTOR } = process.env
 
+console.log('Publishing to GH Pages')
 ghpages.publish(
   'docs',
   {
     branch: 'gh-pages',
-    repo: `https://${ACTIONS_DEPLOY_KEY}:x-oauth-basic@github.com/${GITHUB_REPOSITORY}.git`,
+    repo: `https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git`,
     message: `docs: v${version} release`,
     user: {
       name: GITHUB_ACTOR,
@@ -20,5 +21,6 @@ ghpages.publish(
     if (err) {
       console.error(err)
     }
+    console.log('Complete')
   },
 )
